0e8d47e28281cdf0f3322262a8fd3f8310bd8759
Alhadji Oumate <alhadjioum0@gmail.com>
2024-09-02 20:55:59 +0100

    feat: delete a quiz

--- app/api/quizzes/[quizId]/route.ts
+++ app/api/quizzes/[quizId]/route.ts
@@ -1,4 +1,5 @@
 import { db } from '@/lib/db';
+import { currentUser } from '@clerk/nextjs/server';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
@@ -17,6 +18,57 @@ export async function GET(
     }
 
     return NextResponse.json({ data: quiz }, { status: 200 });
+  } catch (error: any) {
+    console.error(error.message);
+    return NextResponse.json(
+      { message: 'Internal server error' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { quizId: string } },
+) {
+  try {
+    const user = await currentUser();
+
+    if (!user) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const findUser = await db.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!findUser) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    if (findUser.usertype !== 'ADMIN' && findUser.usertype !== 'TEACHER') {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const quiz = await db.quiz.findUnique({
+      where: {
+        id: params.quizId,
+      },
+    });
+
+    if (!quiz) {
+      return NextResponse.json({ message: 'Quiz not found' }, { status: 404 });
+    }
+
+    await db.quiz.delete({
+      where: {
+        id: params.quizId,
+      },
+    });
+
+    return NextResponse.json({ message: 'Quiz deleted' }, { status: 200 });
   } catch (error: any) {
     console.error(error.message);
     return NextResponse.json(
